fa3c4102b156fba10efc626caf74e6a531d92236
kervin521 <zy5211314521@outlook.com>
2018-12-27 14:16:22 +0800

    v1.0

--- src/test/java/com/dev/share/test/SpringXMLKafkaTest.java
+++ src/test/java/com/dev/share/test/SpringXMLKafkaTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.dev.share.kafka.KafkaRecordInfo;
@@ -14,7 +14,7 @@ import com.dev.share.kafka.spring.KafkaProducerService;
 
 public class SpringXMLKafkaTest {
 //	public static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-kafka.xml");
-	public static FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:spring-kafka.xml");
+	public static FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:spring-context.xml");
 	public static ScheduledExecutorService schedule = Executors.newScheduledThreadPool(4);
 	public static void producer() {
 		ctx.registerShutdownHook();
@@ -35,5 +35,8 @@ public class SpringXMLKafkaTest {
 	
 	public static void main(String[] args) {
 		producer();
+//		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+//		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+//		reader.loadBeanDefinitions("spring/applicationContext-test.xml");
 	}
 }
